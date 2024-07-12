d2db2805f226dd849127b84456787135ef67f54a
victor-nsengiyumva <jerrysyre@gmail.com>
2024-07-12 15:43:24 -0700

    approval workflow donr

--- national_id_application/models/national_id_application.py
+++ national_id_application/models/national_id_application.py
@@ -1,29 +1,47 @@
-
 from odoo import models, fields
 
+
 class NationalIDApplication(models.Model):
-    _name = 'national.id.application'
-    _description = 'National Id Application'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _name = "national.id.application"
+    _description = "National Id Application"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
 
-    name = fields.Char(string='Applicant Name', required=True)
-    email = fields.Char(string="Email", required= True)
-    dob = fields.Date(string='Date of Birth', required=True)
-    place_of_birth = fields.Char(string='Place of Birth', required=True)
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', required=True)
-    address = fields.Text(string='Address', required=True)
-    photo = fields.Binary(string='Applicant Photo', attachment=True)
-    lc_reference_letter = fields.Binary(string='LC Reference Letter', attachment=True)
-    state = fields.Selection([('draft', 'Draft'), ('first_approval', 'First Approval'), ('second_approval', 'Second Approval'), ('approved', 'Approved'), ('rejected', 'Rejected')], string='State', default='first_approval', tracking=True)
+    name = fields.Char(string="Applicant Name", required=True)
+    email = fields.Char(string="Email", required=True)
+    dob = fields.Date(string="Date of Birth", required=True)
+    place_of_birth = fields.Char(string="Place of Birth", required=True)
+    gender = fields.Selection(
+        [("male", "Male"), ("female", "Female")], string="Gender", required=True
+    )
+    address = fields.Text(string="Address", required=True)
+    photo = fields.Binary(string="Applicant Photo", attachment=True)
+    lc_reference_letter = fields.Binary(string="LC Reference Letter", attachment=True)
+    state = fields.Selection(
+        [
+            ("pending", "Pending"),
+            ("stage_1", "Stage 1"),
+            ("approved", "Approved"),
+            ("rejected", "Rejected"),
+        ],
+        string="State",
+        default="pending",
+        index=True,
+        copy=False,
+        tracking=True,
+    )
 
-    def action_stage1(self):
-        self.state = 'stage1'
+    def action_first_approval(self):
+        self.write({"state": "stage_1"})
         # self.message_post(body='Moved to Stage 1')
 
-    def action_stage2(self):
-        self.state = 'stage2'
-        # self.message_post(body='Moved to Stage 2')
+    def action_second_approval(self):
+        self.write({"state": "approved"})
 
-    def action_approve(self):
-        self.state = 'approved'
-        # self.message_post(body='Application Approved')
+    # self.message_post(body='Moved to Stage 2')
+
+    # def action_done(self):
+    #     self.write({"state": "approved"})
+    #     # self.message_post(body='Application Approved')
+
+    def reject_application(self):
+        self.write({"state": "rejected"})
